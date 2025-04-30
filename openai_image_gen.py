03d9a498887f213d8d460bab8484abb28e992654
zlava <zlava@gmail.com>
2025-04-30 17:36:27 +0200

    feat: generate image and audio

--- openai_image_gen.py
+++ openai_image_gen.py
@@ -1,4 +1,5 @@
 import base64
+import gradio as gr
 from io import BytesIO
 from PIL import Image
 from openai import OpenAI
@@ -16,10 +17,21 @@ def generate(prompt):
         model="dall-e-3",
         n=1,
     )
-    image_b64=response.data[0].b64_json
+    image_b64 = response.data[0].b64_json
     image_data = base64.b64decode(image_b64)
-    img = Image.open(BytesIO(image_data))
-    img.save("image.png")
+    return Image.open(BytesIO(image_data))
+    # img = Image.open(BytesIO(image_data))
+    # img.save("image.png")
 
 
-generate("A futuristic city skyline at sunset, with flying cars and neon lights")
+# generate("A futuristic city skyline at sunset, with flying cars and neon lights")
+
+with gr.Blocks() as ui:
+    with gr.Row():
+        image = gr.Image(label="Image", height=600)
+    with gr.Row():
+        msg = gr.Textbox(label="Message", placeholder="Enter a message")
+
+    msg.submit(fn=generate, inputs=msg, outputs=image)
+
+ui.launch()
