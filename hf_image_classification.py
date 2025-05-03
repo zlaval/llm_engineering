b88eb4bf8af24edb7186b97c94c4d0c05a34b6aa
zlava <zlava@gmail.com>
2025-05-03 12:19:09 +0200

    feat: image classification

--- hf_image_classification.py
+++ hf_image_classification.py
@@ -1,2 +1,13 @@
+from PIL import Image
+from transformers import ViTImageProcessor, ViTForImageClassification
 
+image = Image.open("assets/dog.jpg")
 
+processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
+inputs = processor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+logits = outputs.logits
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
